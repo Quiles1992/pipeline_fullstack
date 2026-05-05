c6bf2f2907609d5395ba97bd260a002161e49f3a
Quiles1992 <erquilesru@ittepic.edu.mx>
2026-05-04 21:26:16 -0700

    Agregando scrips

---
+++ test.js
@@ -0,0 +1,26 @@
+const e = require('cors');
+const http = require('http');
+
+const options = {
+  hostname: 'localhost',
+  port: 3000,
+  path: '/api/products',
+  method: 'GET'
+};
+
+const req = http.request(options, (res) => {
+    if (res.statusCode === 403 || res.statusCode === 401) {
+        console.log('La api protegida.');
+        process.exit(0);
+    } else {
+        console.log('Test Fallo.');
+        process.exit(1);
+    }
+});
+
+req.on('error', () => {
+    console.log('Error al conectar con la API.');
+    process.exit(1);
+});
+
+req.end();
